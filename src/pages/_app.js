9824bad71c2af8da28b72a2997f83eb7dea122d8
maxcalidonna <max.calidonna@gmail.com>
2023-09-27 11:43:29 +0200

    3. modifiche iniziali

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -55,9 +55,9 @@ const App = props => {
         <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
         <meta
           name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+          content={`${themeConfig.templateName} – Material Design React Admin Dashboard `}
         />
-        <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+        <meta name='keywords' content='Material Design, MUI, React' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
 
